9c0662cfb14eed81820f2ae83d171fd5cf78a76f
Yeong-Kwon Yun <103921408+yk9221@users.noreply.github.com>
2025-01-13 08:50:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -33,9 +33,6 @@ def process_text():
     data = request.get_json()
     sentence = data.get('text').split('.')
 
-
-    app_id = "d5fd616b"
-    app_key = "5c06fb4f5bd545920f9d620b5a5a63d4"
     queries = get_food(sentence)
     print(queries)
     queries_for_nutrients = auto_complete_api_request(queries, app_id, app_key)
